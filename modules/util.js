fc395db8dbbece2b12a6cd6570a3ac2195a4c8de
Tritonn204 <tritonn204@gmail.com>
2022-04-06 16:49:53 -0500

    radians to degrees

--- modules/util.js
+++ modules/util.js
@@ -18,13 +18,12 @@ const angleFromPoints = (cx, cy, ex, ey) => {
   var dy = ey - cy;
   var dx = ex - cx;
   var theta = Math.atan2(dy, dx); // range (-PI, PI]
-  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
   //if (theta < 0) theta = 360 + theta; // range [0, 360)
   return theta;
 }
 
 const angleToVel = (theta) => {
-    return new Vec2(Math.cos(theta), Math.sin(theta));
+    return new Vec2(Math.cos(theta)+1, Math.sin(theta)+1);
 }
 
 class Matrix {
