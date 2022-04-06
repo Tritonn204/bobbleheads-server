63319ddddcce365a104b0ce9f05768e64b207edc
Tritonn204 <tritonn204@gmail.com>
2022-04-06 16:38:53 -0500

    as

--- modules/entity.js
+++ modules/entity.js
@@ -120,11 +120,11 @@ class Entity {
                 case 'dashAttack':
                     this.hp -= 50;
                     this.ragdoll = true;
-                    newVel = angleToVel(angleFromPoints(cantidate.pos, this.pos));
+                    newVel = angleToVel(angleFromPoints(cantidate.pos.x, cantidate.pos.y, this.pos.x, this.pos.y));
                     this.vel.x = this.getImpactVelocity()*newVel.x;
                     this.vel.y = this.getImpactVelocity()*newVel.y;
 
-                    console.log(this.vel);
+                    console.log(newVel);
 
                     if (newVel.y < 0) this.isGrounded = false;
                     break;
