99c36054e01c6205ed3f1f5503b4a5a0cc1ce44a
Tritonn204 <tritonn204@gmail.com>
2022-04-08 10:46:12 -0500

    Test 5

--- server.js
+++ server.js
@@ -280,7 +280,7 @@ Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
                         }
                     }
                 }
-                if (Object.keys(pack).length > 0 && io.sockets.adapter.rooms[room]){
+                if (Object.keys(pack).length > 0){
                     await io.in(room).fetchSockets().forEach(socket => {
                         socket.volatile.emit('remoteData', pack);
                     })
